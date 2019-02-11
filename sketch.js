1418daa4d1a633690726252cd1ee7f6adb6a3386
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-02-10 19:45:52 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+let x,y; 						/* x and y positions */
+let xspeed,yspeed;  /* speed the logo moves */
+let dvd;					  /* dvd logo image */
+let r,g,b;					/* rgb color values */
+
+/* Load the image into the dvd variable */
+function preload() {
+  dvd = loadImage("dvd_logo.png");
+}
+
+/* The image will start at a random x and y position */
+function setup() {
+  createCanvas(1600, 1200);
+	x = random(width);
+	y = random(height);
+	
+	xspeed = 1;
+	yspeed = 1;
+  pickColor();
+}
+
+/* Each time the image hits a wall, it will change color */
+function pickColor() {
+  r = random(256);
+  g = random(256);
+  b = random(256);
+}
+
+/* Draws the image and handles wall collision */
+function draw() {
+  background(0);
+  tint(r,g,b);
+	image(dvd, x, y);
+	
+	x += xspeed;
+	y += yspeed;
+	
+	if (x + dvd.width >= width) {
+		xspeed *= -1;
+    x = width - dvd.width;
+    pickColor();
+	}
+	else if (x <= 0) {
+		xspeed *= -1;
+    x = 0;
+    pickColor();
+	}
+	if (y + dvd.height >= height) {
+		yspeed *= -1;
+    y = height - dvd.height;
+    pickColor();
+	}
+	else if (y <= 0) {
+		yspeed *= -1;
+    y = 0;
+    pickColor();
+	}
+}
